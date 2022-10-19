9c470f9799b24ed2e4b46c7878c96a1b14f80eb6
Jaredslocker21 <jaredslocker21@gmail.com>
2022-10-19 15:12:25 +0000

    sdfg

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -32,7 +32,7 @@ DEBUG = development
 if development:
     ALLOWED_HOSTS = ['localhost']
 else:
-    ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')]
+    ALLOWED_HOSTS = [os.environ.get('jaredslocker-django-todo.herokuapp.com')]
 
 
 # Application definition
